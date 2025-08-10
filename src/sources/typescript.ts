d1ab0f92be30d8bbb743604367c57a9048febe3d
Alireza Safaierad <frontendmonster@gmail.com>
2025-08-10 18:00:20 +0200

    feat: add typescript rules

---
+++ src/sources/typescript.ts
@@ -0,0 +1,29 @@
+import type { TokenColor } from '../types.ts';
+
+import { Foregrounds } from '../colors.ts';
+
+export const typescript: TokenColor[] = [
+  {
+    name: 'Type Parameter',
+    scope: 'comment.line.triple-slash.directive.ts',
+    settings: {
+      foreground: Foregrounds.Neutral,
+      fontStyle: '',
+    },
+  },
+  {
+    name: 'Type Parameter',
+    scope: 'entity.name.label.tsx',
+    settings: {
+      fontStyle: 'bold',
+    },
+  },
+  {
+    name: 'TypeScript Keywords',
+    scope:
+      'keyword.operator.expression.keyof.tsx, keyword.operator.expression.typeof.tsx',
+    settings: {
+      foreground: Foregrounds.Red,
+    },
+  },
+];
