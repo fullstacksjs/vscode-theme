caca66301e8d5123705fc865e9f877e89016bdf3
Alireza Safaierad <frontendmonster@gmail.com>
2025-08-10 19:07:07 +0200

    fix: fix markup font style

--- src/sources/markup.ts
+++ src/sources/markup.ts
@@ -2,9 +2,9 @@ import type { TokenColor } from '../types.ts';
 
 import { Foregrounds } from '../colors.ts';
 
-export const jsx: TokenColor[] = [
+export const xml: TokenColor[] = [
   {
-    name: 'Tag',
+    name: 'JSX Components',
     scope: 'support.class.component.tsx, support.class.component.js.jsx',
     settings: {
       foreground: Foregrounds.Orange,
@@ -16,6 +16,14 @@ export const jsx: TokenColor[] = [
     scope: 'punctuation.section.embedded',
     settings: {
       foreground: Foregrounds.Red,
+    },
+  },
+  {
+    name: 'Tags',
+    scope:
+      'entity.name.tag.localname.xml, entity.name.tag.html, entity.name.tag.tsx',
+    settings: {
+      fontStyle: 'bold',
     },
   },
 ];
