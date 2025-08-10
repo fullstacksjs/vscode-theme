9a53191bdc575a0c7f23c50de2f30a16d5374bda
Alireza Safaierad <frontendmonster@gmail.com>
2025-08-10 17:43:00 +0200

    feat: add colors

---
+++ src/colors.ts
@@ -0,0 +1,46 @@
+const Palette = {
+  Orange: "#ffa478",
+  Yellow: "#ffd57a",
+  Blue: "#35eeff",
+  Cyan: "#14FFDE",
+  Green: "#c6ff90",
+  White: "#ffffff",
+  Red: "#ff7272",
+};
+
+export const Foregrounds = {
+  Orange: Palette.Orange,
+  Yellow: Palette.Yellow,
+  Blue: Palette.Blue,
+  Cyan: Palette.Cyan,
+  Green: Palette.Green,
+  Neutral: Palette.White,
+  Muted: "#ffffff44",
+  Red: Palette.Red,
+};
+
+export const UIColors = {
+  Background: "#23252e",
+  Border: "#1c1e25",
+  Transparent: "#00000000",
+  Foreground: Palette.White,
+};
+
+export const ShellColors = {
+  White: "#f8f8f8",
+  BrightWhite: Palette.White,
+  Black: "#313744",
+  BrightBlack: "#556570",
+  Red: "#e17e85",
+  BrightRed: Palette.Red,
+  Yellow: "#f6ca52",
+  BrightYellow: "#ffd04f",
+  Blue: "#35eeff",
+  BrightBlue: "#4cb2ff",
+  Cyan: "#2dced0",
+  BrightCyan: "#24e8ea",
+  Green: "#5ecb8c",
+  BrightGreen: "#5bf29a",
+  Magenta: "#9292ff",
+  BrightMagenta: "#a7a7ff",
+};
