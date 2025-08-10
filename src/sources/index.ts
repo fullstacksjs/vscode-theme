f76cc3766003ebddaca23ba76cadf5e1bf8e7522
Alireza Safaierad <frontendmonster@gmail.com>
2025-08-10 18:02:17 +0200

    feat: add regex rules

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -2,5 +2,6 @@ export * from './base.ts';
 export * from './css.ts';
 export * from './json.ts';
 export * from './jsx.ts';
+export * from './regex.ts';
 export * from './typescript.ts';
 export * from './yaml.ts';
---
+++ src/sources/regex.ts
@@ -0,0 +1,31 @@
+import type { TokenColor } from '../types.ts';
+
+import { Foregrounds } from '../colors.ts';
+
+export const regex: TokenColor[] = [
+  {
+    name: 'Regex',
+    scope: 'string.regexp',
+    settings: {
+      foreground: Foregrounds.Yellow,
+      fontStyle: 'bold',
+    },
+  },
+  {
+    name: 'Regex Groups',
+    scope:
+      'punctuation.definition.group.regexp, punctuation.definition.character-class.regexp',
+    settings: {
+      foreground: Foregrounds.Red,
+    },
+  },
+  {
+    name: 'Regex Operators',
+    scope:
+      'constant.other.character-class.regexp, keyword.operator.quantifier.regexp,keyword.operator.or.regexp, punctuation.definition.group.assertion.regexp',
+    settings: {
+      foreground: Foregrounds.Red,
+      fontStyle: 'bold',
+    },
+  },
+];
