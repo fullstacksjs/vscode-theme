0eb585115eab11cd69b7503f9d4adf0af1f50321
Alireza Safaierad <frontendmonster@gmail.com>
2025-08-12 17:09:00 +0200

    feat: add status-bar colors

---
+++ src/ui/status-bar.ts
@@ -0,0 +1,23 @@
+import type { UIColor } from '../types.ts';
+
+import { Foregrounds, UIColors } from '../colors.ts';
+
+export const statusBar: UIColor = {
+  'statusBar.foreground': Foregrounds.Neutral,
+  'statusBar.background': UIColors.Background,
+  'statusBar.border': UIColors.Transparent,
+
+  'statusBar.debuggingBackground': UIColors.Danger,
+  'statusBar.debuggingForeground': Foregrounds.OnDanger,
+  'statusBar.debuggingBorder': UIColors.Transparent,
+
+  'statusBar.noFolderBackground': UIColors.Primary,
+  'statusBar.noFolderForeground': Foregrounds.OnPrimary,
+  'statusBar.noFolderBorder': UIColors.Transparent,
+
+  'statusBarItem.hoverBackground': UIColors.BackgroundHover,
+  'statusBarItem.activeBackground': UIColors.FocusBackground,
+
+  'statusBarItem.remoteBackground': UIColors.Background,
+  'statusBarItem.remoteForeground': Foregrounds.Magenta,
+};
